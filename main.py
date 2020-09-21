53f87b0385d6e161180b893b3ace73f76245ed50
LanskovNV <feitrich@yandex.ru>
2020-09-21 16:20:25 +0300

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+# import numpy as np
 
 
 def register_launch_arguments():
@@ -26,11 +27,62 @@ def save_ans(output_file, triangles, error):
         writer = csv.writer(output)
         writer.writerows(data)
 
-def sort_graham(vertices):
-    return []
 
-def earcut(vertices, index_sorted):
-    return []
+# vertices shoutd go in counterclock-wise
+def earcut(vertices):
+    triangles = []
+    ears = []
+
+    def find_all_ears():
+        i = 0
+        length = len(vertices)
+        while True:
+            if i >= length:
+                break
+            if is_convex(vertices[i]): 
+                ears.append(i)
+                i += 2
+
+    def convert_bounds(ind):
+        length = len(vertices)
+        right_neighbour_ind = (ind + 1) % length
+        if ind == 0:
+            left_neighbour_ind = length - 1
+        else: 
+            left_neighbour_ind = ind - 1
+        return left_neighbour_ind, right_neighbour_ind
+
+    def is_convex(vert):
+        return True
+
+
+    #TODO: fix this func
+    def update_ears(ind):
+        left_neighbour_ind, right_neighbour_ind = convert_bounds(ind)
+        if is_convex(vertices[left_neighbour_ind]):
+            ears.append(left_neighbour_ind)
+            return
+        if is_convex(vertices[right_neighbour_ind]):
+            ears.append(right_neighbour_ind)
+            return
+
+    def add_triangle(ind):
+        left_neighbour_ind, right_neighbour_ind = convert_bounds(ind)
+        triangles.append([left_neighbour_ind, ind, right_neighbour_ind])
+
+    def cut_ear():
+        ear_ind = ears.pop(0)
+        add_triangle(ear_ind)
+        update_ears(ear_ind)
+
+    find_all_ears()
+    while True:
+        if len(ears) == 1:
+            add_triangle(ears[0])
+            break    
+        cut_ear()
+
+    return triangles
 
 if __name__ == '__main__':
     args = register_launch_arguments()
@@ -38,8 +90,7 @@ if __name__ == '__main__':
 
     try:
         vertices = parse_input(args.input)
-        index_sorted = sort_graham(vertices)
-        triangles = earcut(vertices, index_sorted)
+        triangles = earcut(vertices)
     except ValueError:
         error = 'ERROR: Incorrect input'
 
